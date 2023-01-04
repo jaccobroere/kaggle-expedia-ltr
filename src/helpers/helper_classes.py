98ad690d4ed3dd85c9cebee37b5f7a81a16da290
jaccobroere <jacbroere@gmail.com>
2023-01-04 16:42:34 +0100

    trying it with csv.writer module

--- src/helpers/helper_classes.py
+++ src/helpers/helper_classes.py
@@ -3,6 +3,7 @@ from typing import Optional
 import os
 import csv
 from pandas.errors import EmptyDataError
+import dask.dataframe as dd
 
 
 def get_columns(fname: str) -> list:
@@ -21,7 +22,6 @@ class OutsideMemoryDF:
     This is done by transposing the dataframe and writing the columns as rows."""
 
     def __init__(self, fname_write: str, fname_read: str):
-        super().__init__()
         self.fname_write = fname_write
         self.fname_read = fname_read
 
@@ -33,15 +33,17 @@ class OutsideMemoryDF:
     def columns_write(self):
         return get_columns(self.fname_write)
 
-    def write_col(self, col, colname: Optional[str] = None) -> None:
-        (col.T).to_csv(self.fname_write, mode="a", index_col=colname)
+    def write_column(self, df, col) -> None:
+        with open(self.fname_write, "a") as f:
+            writer = csv.writer(f)
+            writer.writerow([col] + df[col].tolist())
 
     def read_col(
         self,
         colname: Optional[str],
         use_idx: Optional[bool] = False,
         idx_col: Optional[int] = None,
-    ):
+    ) -> pd.DataFrame:
         if use_idx:
             return pd.read_csv(self.fname_read, usecols=[idx_col])
         else:
@@ -49,4 +51,9 @@ class OutsideMemoryDF:
 
 
 if __name__ == "__main__":
-    df = OutsideMemoryDF(fname_read="data/train.csv", fname_write="data/tryingoom.csv")
+    df = OutsideMemoryDF(fname_read="data/train.csv", fname_write="data/tryingoom2.csv")
+
+    print("starting")
+    a, b = df.read_col("price_usd"), df.read_col("srch_room_count")
+
+    df.write_column(a, "price_usd")
