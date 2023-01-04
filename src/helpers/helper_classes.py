ac49c0fd5d08f0e73b79e51a54d8629737322aa5
jaccobroere <jacbroere@gmail.com>
2023-01-04 15:00:56 +0100

    trying to solve it by transposign

--- src/helpers/helper_classes.py
+++ src/helpers/helper_classes.py
@@ -17,7 +17,8 @@ def get_columns(fname: str) -> list:
 
 
 class OutsideMemoryDF:
-    """A class that allows you to read and write columns of a csv file without loading the entire file into memory."""
+    """A class that allows you to read and write columns of a csv file without loading the entire file into memory.
+    This is done by transposing the dataframe and writing the columns as rows."""
 
     def __init__(self, fname_write: str, fname_read: str):
         super().__init__()
@@ -32,9 +33,8 @@ class OutsideMemoryDF:
     def columns_write(self):
         return get_columns(self.fname_write)
 
-    # def write_col(self, col):
-
-    #     self.to_csv(self.fname_write, columns=[colname])
+    def write_col(self, col, colname: Optional[str] = None) -> None:
+        (col.T).to_csv(self.fname_write, mode="a", index_col=colname)
 
     def read_col(
         self,
